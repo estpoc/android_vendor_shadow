c5e2b21e50d2e2d7bcb476a655b8c26c4d316296
Edgard Castro <edgardcastro@gmail.com>
2011-01-13 04:37:12 -0200

    Update gmsversion

--- products/shadow_umts_sholes.mk
+++ products/shadow_umts_sholes.mk
@@ -59,7 +59,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.call_ring.multiple=false \
     ro.telephony.call_ring.delay=3000 \
     ro.setupwizard.enable_bypass=1 \
-    ro.com.google.gmsversion=2.2_r8 \
+    ro.com.google.gmsversion=2.2_r9 \
     ro.modversion=SMBR-0.9.15
 
 # ShadowMOD specific product packages
